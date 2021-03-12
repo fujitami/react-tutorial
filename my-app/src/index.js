eda8fd90ad628ea3a9db713545ca1b3ff54a5a7b
fujitami <midori.fujita.0423@gmail.com>
2021-03-13 02:20:41 +0900

    local dev

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './tictachoe.css';
+
 function Square(props) {
   return (
     <button className="square" onClick={props.onClick}>
